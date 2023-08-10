a9d3490436d6f9da15569117267bd9aa6ac23cf7
RajGupta-0904 <114810532+RajGupta-0904@users.noreply.github.com>
2023-08-10 12:27:10 +0530

    Time: 0 ms (100.00%), Space: 43 MB (8.03%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,26 +14,26 @@
  * }
  */
 class Solution {
-    public int diameterOfBinaryTree(TreeNode root) {
-        if(root==null){
-            return 0;
-        }
-        int lh= diameterOfBinaryTree(root.left);
-        int rh= diameterOfBinaryTree(root.right);
-        int sd=height(root.left)+height(root.right)+2;
-        return Math.max(lh,Math.max(rh,sd));
-    }
-    
-    public static int height(TreeNode m) {
-		if(m==null) {
-			return -1;
-			//single node ki height 0 aayega
-//			single node ki height 1 hoga if we return 0
+//     complexity becomes o(n)
+		    public int diameterOfBinaryTree(TreeNode root) {
+		    	return diameter(root).dia;
+		    }
+		    
+		    public diapair diameter(TreeNode root) {
+		    	if(root==null) {
+		    		return new diapair();
+		    		}
+		    	diapair ldp=diameter(root.left);
+		    	diapair rdp=diameter(root.right);
+		    	int sd=ldp.ht+rdp.ht+2;
+		    	diapair sdp =new diapair();
+		    	sdp.dia=Math.max(sd,Math.max(ldp.dia, rdp.dia));
+		    	sdp.ht=Math.max(ldp.ht, rdp.ht)+1;
+		    	return sdp;
+		    	
+		    }
 		}
-		
-		int lh=height(m.left);
-		int rh=height(m.right);
-		return Math.max(lh, rh)+1;
-	}
-    
-}
+		class diapair{
+			int dia=0;
+			int ht=-1;
+		}
