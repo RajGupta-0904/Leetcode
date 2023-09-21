6b9f32797e6ab743e48eb0705f539d8b5d05ccbd
RajGupta-0904 <114810532+RajGupta-0904@users.noreply.github.com>
2023-09-21 11:35:41 +0530

    Time: 39 ms (45.61%), Space: 56.9 MB (55.77%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,22 +1,52 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        if(nums.length<2){
-            return nums.length;
+//         log(n) solution
+        // if(nums.length<2){
+        //     return nums.length;
+        // }
+        // Arrays.sort(nums);
+        // int max=Integer.MIN_VALUE;
+        // int c=1;
+        // for(int i=1;i<nums.length;i++){
+        //     if(nums[i]==nums[i-1]+1){
+        //         c++;
+        //     }
+        //    else if(nums[i]!=nums[i-1]){
+        //         c=1;
+        //     }
+        //     if(c>max){
+        //         max=c;
+        //     }
+        // }
+        // return max==Integer.MIN_VALUE?1:max;
+        return solve(nums);
+    }
+    public static int solve(int []arr){
+        HashMap<Integer,Boolean> map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            if(map.containsKey(arr[i]-1)){
+                map.put(arr[i],false);
+            }
+            else{
+                map.put(arr[i],true);           
+            }
+            if(map.containsKey(arr[i]+1)){
+                map.put(arr[i]+1,false);
+            }
         }
-        Arrays.sort(nums);
-        int max=Integer.MIN_VALUE;
-        int c=1;
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]==nums[i-1]+1){
-                c++;
+        int ans=0;
+        for(int key : map.keySet()){
+            if(map.get(key)){
+                int count=0;
+                    while(map.containsKey(key)){
+                        count++;
+                        key++;
+                    }
+                ans=Math.max(ans,count);
             }
-           else if(nums[i]!=nums[i-1]){
-                c=1;
-            }
-            if(c>max){
-                max=c;
-            }
+            
+            
         }
-        return max==Integer.MIN_VALUE?1:max;
+        return ans;
     }
 }
