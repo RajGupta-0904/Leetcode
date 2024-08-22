ae9a7a25397d01437c9a0a749fcbfb55f330edc0
RajGupta-0904 <114810532+RajGupta-0904@users.noreply.github.com>
2024-08-22 09:18:37 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (67.03%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findComplement(int num) {
+        int i =0,ans=0;
+        while(num!=0){
+        if((num&1)==0)
+            ans+=1<<i;
+            i++;
+            num>>=1;
+        
+        }
+        return ans;
+    }
+}
