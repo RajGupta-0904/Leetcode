703ffedeabf0c58793f362a1f711d23a08b20d45
RajGupta-0904 <114810532+RajGupta-0904@users.noreply.github.com>
2024-08-24 23:16:08 +0530

    Time: 1 ms (100.00%), Space: 47.8 MB (40.84%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,19 +1,21 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        return answer(nums,goal);
+       int a=solve(nums,goal);
+        int b=solve(nums,goal-1);
+        return Math.abs(b-a);
     }
-        public static int answer (int []raj,int k){
-        int count =0;
-        for(int i=0;i<raj.length;i++){
-            int sum=0;
-            for(int j=i;j<raj.length;j++){
-                sum=sum+raj[j];
-                if(sum==k){
-                count++;
+    public static int solve(int []nums,int goal){
+        if(goal<0) return 0;
+         int si=0,ei=0,ans=0,sum=0;
+        while(ei<nums.length){
+            sum+=nums[ei];
+            while(sum>goal && si<=ei){
+                sum-=nums[si];
+                si++;
             }
-            }
-            
+            ans+=ei-si+1;
+            ei++;
         }
-        return count;
+        return ans;
     }
 }
