71ba64224eb8b2e4e03d4d0ce83d4b7a598050e8
RajGupta-0904 <114810532+RajGupta-0904@users.noreply.github.com>
2024-10-15 11:24:03 +0530

    Time: 8 ms (45.52%), Space: 45.3 MB (54.12%) - LeetHub

---
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long minimumSteps(String s) {
+        long ans=0,black=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='0'){
+                ans+=black;
+            }else
+            black++;
+        }
+        return ans;
+    }
+}
